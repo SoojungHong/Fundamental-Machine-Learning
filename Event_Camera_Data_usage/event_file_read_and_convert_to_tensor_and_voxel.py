1239b325f8dae9efa7b09d264f68a7a45ea6ffc0
Soojung Hong <hong.soojung@gmail.com>
2023-04-06 22:37:28 +0200

    Create event_file_read_and_convert_to_tensor_and_voxel.py

---
+++ Event_Camera_Data_usage/event_file_read_and_convert_to_tensor_and_voxel.py
@@ -0,0 +1,110 @@
+#--------------------------
+# Read event file (.txt) 
+#--------------------------
+class FixedSizeEventReader:
+    """
+    Reads events from a '.txt' or '.zip' file, and packages the events into
+    non-overlapping event windows, each containing a fixed number of events.
+    """
+
+    def __init__(self, path_to_event_file, num_events=10000, start_index=0):
+        print('Will use fixed size event windows with {} events'.format(num_events))
+        print('Output frame rate: variable')
+        self.iterator = pd.read_csv(path_to_event_file, delim_whitespace=True, header=None,
+                                    names=['t', 'x', 'y', 'pol'],
+                                    dtype={'t': np.float64, 'x': np.int16, 'y': np.int16, 'pol': np.int16},
+                                    engine='c',
+                                    skiprows=start_index + 1, chunksize=num_events, nrows=None, memory_map=True)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with Timer('Reading event window from file'):
+            event_window = self.iterator.__next__().values
+        return event_window
+
+
+#-----------------------------------------------
+# Event (NumPy array) to Voxel grid (tensor)
+#-----------------------------------------------
+def events_to_voxel_grid_pytorch(events, num_bins, width, height, device):
+    """
+    Build a voxel grid with bilinear interpolation in the time domain from a set of events.
+
+    :param events: a [N x 4] NumPy array containing one event per row in the form: [timestamp, x, y, polarity]
+    :param num_bins: number of bins in the temporal axis of the voxel grid
+    :param width, height: dimensions of the voxel grid
+    :param device: device to use to perform computations
+    :return voxel_grid: PyTorch event tensor (on the device specified)
+    """
+
+    DeviceTimer = CudaTimer if device.type == 'cuda' else Timer
+
+    assert(events.shape[1] == 4)
+    assert(num_bins > 0)
+    assert(width > 0)
+    assert(height > 0)
+
+    with torch.no_grad():
+
+        events_torch = torch.from_numpy(events)
+        with DeviceTimer('Events -> Device (voxel grid)'):
+            events_torch = events_torch.to(device)
+
+        with DeviceTimer('Voxel grid voting'):
+            voxel_grid = torch.zeros(num_bins, height, width, dtype=torch.float32, device=device).flatten()
+
+            # normalize the event timestamps so that they lie between 0 and num_bins
+            last_stamp = events_torch[-1, 0]
+            first_stamp = events_torch[0, 0]
+            deltaT = last_stamp - first_stamp
+
+            if deltaT == 0:
+                deltaT = 1.0
+
+            events_torch[:, 0] = (num_bins - 1) * (events_torch[:, 0] - first_stamp) / deltaT
+            ts = events_torch[:, 0]
+            xs = events_torch[:, 1].long()
+            ys = events_torch[:, 2].long()
+            pols = events_torch[:, 3].float()
+            pols[pols == 0] = -1  # polarity should be +1 / -1
+
+            tis = torch.floor(ts)
+            tis_long = tis.long()
+            dts = ts - tis
+            vals_left = pols * (1.0 - dts.float())
+            vals_right = pols * dts.float()
+
+            valid_indices = tis < num_bins
+            valid_indices &= tis >= 0
+            voxel_grid.index_add_(dim=0,
+                                  index=xs[valid_indices] + ys[valid_indices]
+                                  * width + tis_long[valid_indices] * width * height,
+                                  source=vals_left[valid_indices])
+
+            valid_indices = (tis + 1) < num_bins
+            valid_indices &= tis >= 0
+
+            voxel_grid.index_add_(dim=0,
+                                  index=xs[valid_indices] + ys[valid_indices] * width
+                                  + (tis_long[valid_indices] + 1) * width * height,
+                                  source=vals_right[valid_indices])
+
+        voxel_grid = voxel_grid.view(num_bins, height, width)
+
+    return voxel_grid      
+      
+#------------------------------------------------------------
+# read event file (.txt) load and form event voxel tensor 
+#------------------------------------------------------------
+event_window_iterator = FixedSizeEventReader(path_to_events, num_events=N, start_index=start_index)
+      
+for event_window in event_window_iterator:
+  last_timestamp = event_window[-1, 0]
+  with Timer('Building event tensor'):
+    if args.compute_voxel_grid_on_cpu:
+        event_tensor = events_to_voxel_grid(event_window, num_bins=model.num_bins, width=width, height=height)
+        event_tensor = torch.from_numpy(event_tensor)
+    else:
+        event_tensor = events_to_voxel_grid_pytorch(event_window, num_bins=model.num_bins, width=width, height=height, device=device)
